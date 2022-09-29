bd3819b648fd23eb42029a03b766d32189f8e97c
ielouazz <113651526+ielouazz@users.noreply.github.com>
2022-09-29 00:38:09 -0400

    callMethod

--- main.m
+++ main.m
@@ -1,16 +1,41 @@
-//
-//  main.m
-//  helloWorld
-//
-//  Created by Ikram Elouazzani on 9/29/22.
-//
-
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+@interface SampleClass:NSObject
+/* method declaration */
+- (int)max:(int)num1 andNum2:(int)num2;
+@end
+
+@implementation SampleClass
+
+/* method returning the max between two numbers */
+- (int)max:(int)num1 andNum2:(int)num2 {
+
+   /* local variable declaration */
+   int result;
+ 
+   if (num1 > num2) {
+      result = num1;
+   } else {
+      result = num2;
+   }
+ 
+   return result;
+}
+
+@end
+
+int main () {
+   
+   /* local variable definition */
+   int a = 100;
+   int b = 200;
+   int ret;
+   
+   SampleClass *sampleClass = [[SampleClass alloc]init];
+
+   /* calling a method to get max value */
+   ret = [sampleClass max:a andNum2:b];
+ 
+   NSLog(@"Max value is : %d\n", ret );
+   return 0;
 }
